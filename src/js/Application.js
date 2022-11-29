d1a7703c803fd8a4c079c6979e1aba2f927a9cd5
PaFkaTa133 <119074272+PaFkaTa133@users.noreply.github.com>
2022-11-29 12:11:10 +0200

    do nqkyde

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,28 +1,38 @@
 import EventEmitter from "eventemitter3";
 
 export default class Application extends EventEmitter {
-  static get events() {
-    return {
-      READY: "ready",
-    };
-  }
+    static get events() {
+        return {
+            READY: "ready",
+        };
+    }
 
-  constructor() {
-    super();
-    this.emojis = [];
-    this.banana = "🍌";
-    this.emit(Application.events.READY);
-  }
-  setEmojis(emojis) {
-    this.emojis = emojis;
-    emojis.forEach(emojis => {
-      console.log(emojis+this.banana);
-    });
-    
-    
-  }
+    constructor() {
+        super();
+        this.emojis = [];
+        this.banana = "🍌";
+        this.emit(Application.events.READY);
+    }
+    setEmojis(emojis) {
+        this.emojis = emojis;
+        //document.getElementById("emojis").innerHTML = emojis;
 
-  addBananas() {
-    
-   }
+
+    }
+
+    addBananas() {
+        const elem = document.createElement("p");
+        document.getElementById("emojis").appendChild(elem);
+        const newArr = [];
+        newArr.map();
+
+        function myFunction(item, index, arr) {
+            arr[index] = emojis + banana;
+        }
+
+
+
+
+
+    }
 }
