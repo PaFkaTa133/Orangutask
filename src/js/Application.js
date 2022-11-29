0fb3ea0319d419adec7bb4a431f3aed563d0aa99
PaFkaTa133 <119074272+PaFkaTa133@users.noreply.github.com>
2022-11-29 21:50:16 +0200

    Update Application.js

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,38 +1,26 @@
 import EventEmitter from "eventemitter3";
 
 export default class Application extends EventEmitter {
-    static get events() {
-        return {
-            READY: "ready",
-        };
-    }
+  static get events() {
+    return {
+      READY: "ready",
+    };
+  }
 
-    constructor() {
-        super();
-        this.emojis = [];
-        this.banana = "🍌";
-        this.emit(Application.events.READY);
-    }
-    setEmojis(emojis) {
-        this.emojis = emojis;
-        //document.getElementById("emojis").innerHTML = emojis;
+  constructor() {
+    super();
+    this.emojis = [];
+    this.banana = "🍌";
+    this.emit(Application.events.READY);
+  }
+  setEmojis(emojis) {
+    this.emojis = emojis;
+  }
 
-
-    }
-
-    addBananas() {
-        const elem = document.createElement("p");
-        document.getElementById("emojis").appendChild(elem);
-        const newArr = [];
-        newArr.map();
-
-        function myFunction(item, index, arr) {
-            arr[index] = emojis + banana;
-        }
-
-
-
-
-
-    }
+  addBananas() {
+    const elem = document.createElement("p");
+    document.getElementById("emojis").appendChild(elem);
+    let newArr = this.emojis.map((monkey) => monkey + this.banana);
+    elem.innerHTML = newArr;
+  }
 }
